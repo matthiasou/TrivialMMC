0b89ca4232a835a003fad1344ce00172cc449532
leducqc <charles.leducq@sts-sio-caen.info>
2014-10-02 11:25:57 +0200

    Blabla

--- classes/Probleme.php
+++ classes/Probleme.php
@@ -10,4 +10,38 @@ class Probleme extends \BaseObject{
     private $id;
     private $libelle;
 
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLibelle()
+    {
+        return $this->libelle;
+    }
+
+    /**
+     * @param mixed $libelle
+     */
+    public function setLibelle($libelle)
+    {
+        $this->libelle = $libelle;
+    }
+
+
+
 } 
