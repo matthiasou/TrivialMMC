98a3666067b41173c43f08d7fd72529fde788180
matthiasou <matthiasou@yahoo.fr>
2014-10-02 22:48:30 +0200

    bon

--- views/vHeader.php
+++ views/vHeader.php
@@ -8,6 +8,6 @@ require_once 'technics/Gui.php';
 <script type="text/javascript" src="<?php echo $GLOBALS["siteUrl"]?>js/jquery-2.0.3.js"></script>
 <script type="text/javascript" src="<?php echo $GLOBALS["siteUrl"]?>js/jquery.validate.js"></script>
 <link rel="stylesheet" type="text/css" href="<?php echo $GLOBALS["siteUrl"]?>css/main.css">
-<title>TriviaPro</title>
+<title>TriviaMMC</title>
 </head>
 <body>
