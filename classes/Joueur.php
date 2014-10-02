50c19de6b91d9ed94e5bfb6907daf2d5d414d973
matthiasou <matthiasou@yahoo.fr>
2014-10-02 11:19:23 +0200

    1commit

--- classes/Joueur.php
+++ classes/Joueur.php
@@ -4,17 +4,77 @@ class Joueur extends \BaseObject{
 	private $nom;
 	private $prenom;
 	private $mail;
+    private $login;
+    private $password;
+    private $niveau;
+
+
+
+    /**
+     * @OneToMany(mappedBy="joueur1",className="Score")
+     */
+    private $scores=array();
+
+    /**
+     * @ManyToOne
+     * @JoinColumn(name="idMonde",className="Monde")
+     */
+    private $monde;
+
+    /**
+     * @OneToMany(mappedBy="joueur1",className="Partie")
+     */
+    private $parties=array();
+
+    /**
+     * @return mixed
+     */
+    public function getLogin()
+    {
+        return $this->login;
+    }
+
+    /**
+     * @param mixed $login
+     */
+    public function setLogin($login)
+    {
+        $this->login = $login;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNiveau()
+    {
+        return $this->niveau;
+    }
+
+    /**
+     * @param mixed $niveau
+     */
+    public function setNiveau($niveau)
+    {
+        $this->niveau = $niveau;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
 	
-	/**
-	 * @ManyToOne
-	 * @JoinColumn(name="idMonde",className="Monde")
-	 */
-	private $monde;
-	
-	/**
-	 * @OneToMany(mappedBy="joueur1",className="Partie")
-	 */
-	private $parties=array();
+
 
 	public function getNom() {
 		return $this->nom;
@@ -67,6 +127,22 @@ class Joueur extends \BaseObject{
 	public function toString() {
 		return $this->nom." ".$this->prenom;
 	}
+
+    /**
+     * @return mixed
+     */
+    public function getScores()
+    {
+        return $this->scores;
+    }
+
+    /**
+     * @param mixed $scores
+     */
+    public function setScores($scores)
+    {
+        $this->scores = $scores;
+    }
 
 	
 }
--- classes/Question.php
+++ classes/Question.php
@@ -1,11 +1,65 @@
 <?php
 /**
  * Created by PhpStorm.
+<<<<<<< Updated upstream
  * User: charlesleducq
  * Date: 02/10/2014
  * Time: 11:11
  */
 
-class Question extends \BaseObject{
+
+class Question extends \BaseObject {
+
+    private $libelle;
+    private $validation;
+
+    /**
+     * @ManyToMany(targetEntity="Joueur", inversedBy="idJoueur1")
+     * @JoinTable(name="Joueur")
+     */
+    private $idJoueur;
+
+
+
+    /**
+     * @ManyToOne
+     * @JoinColumn(name="idDomaine",className="Domaine")
+     */
+    private $idDomaine;
+
+    /**
+     * @return mixed
+     */
+    public function getLibelle()
+    {
+        return $this->libelle;
+    }
+
+    /**
+     * @param mixed $libelle
+     */
+    public function setLibelle($libelle)
+    {
+        $this->libelle = $libelle;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getValidation()
+    {
+        return $this->validation;
+    }
+
+    /**
+     * @param mixed $validation
+     */
+    public function setValidation($validation)
+    {
+        $this->validation = $validation;
+    }
+
+
+
 
 } 
