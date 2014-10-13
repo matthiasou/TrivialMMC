7a1dd043dbd608263c453d6ca7dfb892273d8335
Maxime Binet <paxman2@hotmail.fr>
2014-10-13 11:27:50 +0200

    truccs cools

--- controllers/CJoueur.php
+++ controllers/CJoueur.php
@@ -115,6 +115,11 @@ class CJoueur extends \BaseController{
 
 
     public function listerParties(){
+
+        //$parties=DAO::getOneToMany($_SESSION["joueur1"], "parties");
+        //$this->loadView("vPartie", $parties);
+
+        //Affiche toutes les parties en cours du joueur
         if (isset ($_SESSION['joueur1'])) {
 
 
@@ -124,13 +129,16 @@ class CJoueur extends \BaseController{
             $partiesEnCours = DAO::getAll("Partie", "idJoueur1=" . $idJoueur . " OR idJoueur2=" . $idJoueur);
             // Affiche les parties qui sont possible à rejoindre
             $partiesJoignables = DAO::getAll("Partie", "idJoueur2 is NULL AND idJoueur1 != $idJoueur");
-            //var_dump( DAO::getAll("Partie", "idJoueur1=" . $idJoueur . " OR idJoueur2=" . $idJoueur));
+            //var_dump( DAO::getAll("Partie","idJoueur2 is NULL AND idJoueur1 != $idJoueur"));
 
             $this->loadView("vPartie", array("pEnCours" => $partiesEnCours, "pJoignables" => $partiesJoignables));
             echo JsUtils::getAndBindTo(".rejoindre", "click", "/trivia/CQuestion/randomQuestion", "{}","#divMessage");
-
         }
 
+
+    }
+
+    public function statPartie(){
 
     }
 
--- views/vPartie.php
+++ views/vPartie.php
@@ -2,14 +2,21 @@
     <?php
     echo"Parties joignables :</br></br>";
     foreach ($data["pJoignables"] as $partiesJoignables){
-        echo $partiesJoignables." <a class='rejoindre' href='#' id='rejoindre".$partiesJoignables->getId()."'>Rejoindre</a><br>";
+        echo $partiesJoignables." <a class='rejoindre' href='#' id='rejoindre".$partiesJoignables->getId()."'>Rejoindre</a><br/>";
+        echo JsUtils::getAndBindTo(".rejoindre", "click", "/trivia/CQuestion/randomQuestion", "{}","#divMessage");
     }
 
     echo "</br>Parties en cours :</br></br>";
     foreach ($data["pEnCours"] as $partiesEnCours){
-        echo $partiesEnCours."</br>";
+        if($partiesEnCours == $_SESSION['joueur1']->getNom())
+            echo $partiesEnCours . "<a class='jouer' href='#' id='jouer".$partiesEnCours->getId()."'> Jouer</a></br>";
+    else
+            echo $partiesEnCours . "<br/>";
+        //echo $partiesEnCours."</br>";
     }
+
+    echo JsUtils::getAndBindTo(".jouer", "click", "/trivia/CQuestion/randomQuestion", "{}","#divMessage");
+
     ?>
 </div>
-
 
