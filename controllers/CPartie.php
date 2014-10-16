e64c5df2c85a31b8da2462e3c4f1e965cc445c1b
matthiasou <matthiasou@yahoo.fr>
2014-10-16 11:24:20 +0200

    bon

--- controllers/CPartie.php
+++ controllers/CPartie.php
@@ -18,6 +18,8 @@ class CPartie extends \BaseController {
     public function jouer(){
         // Afficher une question
         // Si bonne autre question sinon à l'autre joueur de jouer
+        $question = new CQuestion();
+        $question->randomQuestion();
 
 
 
