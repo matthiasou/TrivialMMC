0b522970a34cb5eb32a850c2ef63434e78b72205
Maxime <paxman2@hotmail.fr>
2014-10-16 11:25:21 +0200

    "rejoindre partie" fonctionne

--- controllers/CQuestion.php
+++ controllers/CQuestion.php
@@ -10,7 +10,6 @@ class CQuestion extends \BaseController {
     
     public function index(){
         $this->loadView("vHeader");
-        $this->randomQuestion();
 
     }
 
@@ -20,7 +19,9 @@ class CQuestion extends \BaseController {
 
     }
 
-    public function randomQuestion(){
+    public function randomQuestion($params){
+        $idPartie = str_replace("jouer", "", $params[0]);
+        echo $idPartie;
         echo JsUtils::doSomethingOn("#divListe","hide"); // cache le menu principal avec les parties
         $question=DAO::getOne("Question", "1=1 ORDER BY RAND() LIMIT 1");
         DAO::getOneToMany($question, "reponses");
