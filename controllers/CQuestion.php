d1e57462604d7d93e39372e42c93d0edfcdb5c5b
Maxime <paxman2@hotmail.fr>
2014-10-16 12:02:36 +0200

    "rejoindre partie" fonctionne

--- controllers/CQuestion.php
+++ controllers/CQuestion.php
@@ -21,19 +21,25 @@ class CQuestion extends \BaseController {
 
     public function randomQuestion($params){
         $idPartie = str_replace("jouer", "", $params[0]);
-        echo $idPartie;
         echo JsUtils::doSomethingOn("#divListe","hide"); // cache le menu principal avec les parties
         $question=DAO::getOne("Question", "1=1 ORDER BY RAND() LIMIT 1");
         DAO::getOneToMany($question, "reponses");
         //echo $reponses;
         $this->loadView("vQuestion", $question);
-        echo JsUtils::getAndBindTo(".reponse", "click", "/trivia/CQuestion/checkAnswer", "{}", "#messageReponse");
+        echo JsUtils::getAndBindTo(".reponse", "click", "/trivia/CQuestion/checkAnswer/" . $idPartie, "{}", "#messageReponse");
     }
 
     public function checkAnswer($p){
-        $estBonne=DAO::getOne("Reponse",$p[0])->getEstBonne();
-        if($estBonne == 1)
+        var_dump($p);
+        $idJoueur = $_SESSION['joueur1']->getId();
+        $estBonne=DAO::getOne("Reponse",$p[1])->getEstBonne();
+        if($estBonne == 1){
             echo "Bonne réponse";
+            $score = DAO::getOne("Score", "idPartie = " . $p[0] . " AND idJoueur = " . $idJoueur);
+            $score->setNbBonnesReponses($score->getNbBonnesReponses()+1);
+            DAO::update($score);
+
+            }
         else
             echo "Mauvaise réponse";
     }
