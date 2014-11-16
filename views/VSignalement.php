6fc6364c8f6eb1f112f9c86f7fdce6c623e31edb
matthiaslecomte <matthiasou@yahoo.fr>
2014-11-16 16:34:56 +0100

    signalement

--- views/VSignalement.php
+++ views/VSignalement.php
@@ -1,8 +1,9 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: matthiaslecomte
- * Date: 16/11/14
- * Time: 15:07
- */
-echo 'coucou';
+<form id="frmSignalement" name="frmSignalement">
+    <fieldset>
+        <legend>Signaler une question :</legend>
+        <textarea name="signalement" id="signalement"rows="10" cols="50">Saisir un texte ici.</textarea>
+        <input type="button" value="Valider" id="btSignalement">
+    </fieldset>
+</form>
+
+
