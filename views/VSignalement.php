0ed9db0d4a3242b0b987ef4dc66e83407ea840b5
matthiaslecomte <matthiasou@yahoo.fr>
2014-11-16 19:44:32 +0100

    textholder

--- views/VSignalement.php
+++ views/VSignalement.php
@@ -1,7 +1,7 @@
 <form id="frmSignalement" name="frmSignalement">
     <fieldset>
         <legend>Signaler une question :</legend>
-        <textarea name="signalement" id="signalement"rows="10" cols="50">Saisir un texte ici.</textarea>
+        <textarea name="signalement" placeholder="Ecrire votre texte ici" id="signalement"rows="10" cols="50"></textarea>
         <input type="button" value="Valider" id="btSignalement">
     </fieldset>
 </form>
