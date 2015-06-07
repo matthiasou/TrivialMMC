f94b3107a7e1c427c3a0a6a3f0b26dc68dad57fe
matthiaslecomte <matthiasou@yahoo.fr>
2015-06-07 17:19:41 +0200

    modification info partie

--- views/vInfoPartie.php
+++ views/vInfoPartie.php
@@ -1,14 +1,19 @@
-
+<br><br>
     <form id="frmScore" name="frmScore" class="score">
-        <fieldset>
-             <legend>Scores :</legend>
              <?php //var_dump($data)?>
-             Manche N°<?php echo $data["scoreJ1"]->getNbManches();?> </br><br>
-             Bonnes réponses :</br>
-             Vous : <?php echo $data["scoreJ1"]->getNbBonnesReponses(). " Votre adversaire : " .$data["scoreJ2"]->getNbBonnesReponses();?> </br></br>
-             Couronnes débloquées :</br>
-             Vous : <?php echo sizeof($data["couronneJ1"]). " Votre adversaire : " .sizeof($data["couronneJ2"]);?></br></br>
-             Réponses successives :  <?php echo $data["scoreJ1"]->getRepSuccessives();?>
+            <TABLE width="100%">
 
-        </fieldset>
+                <TR>
+                    <TH> Manche N° <?php echo $data["scoreJ1"]->getNbManches();?></TH>
+                    <TH> Réponses successives : <br>  <div id="verte"><?php echo $data["scoreJ1"]->getRepSuccessives();?></div>  </TH>
+                    <TH> Bonnes réponses : <br>Vous : <div id="verte"><?php echo $data["scoreJ1"]->getNbBonnesReponses();?></div> Votre adversaire : <div id="rouge"><?php  echo $data["scoreJ2"]->getNbBonnesReponses();?></div> </TH>
+                    <TH>                    </TH>
+                    <TH> Couronnes débloquées : <br> Vous : <div id="verte"><?php echo sizeof($data["couronneJ1"]);?> </div> Votre adversaire : <div id="rouge"><?php echo sizeof($data["couronneJ2"]);?></div></TH>
+                </TR>
+
+            </TABLE>
+
+
+
+
     </form>
